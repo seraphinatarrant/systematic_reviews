74ac99e84dbf8810e23c271be80b2930e2d9bdae
seraphinatarrant <serif@uw.edu>
2019-11-26 01:02:50 +0000

    adding split data utils function

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -3,6 +3,7 @@ import yaml
 import random
 import string
 from itertools import filterfalse
+from sklearn.model_selection import train_test_split
 
 def read_yaml_config(config_file):
     return yaml.load(open(config_file), Loader=yaml.FullLoader)
@@ -24,3 +25,7 @@ def make_id():
     id_length = 10 # we can change this to be longer based on volume of crawl
     alphanumeric = string.ascii_lowercase + string.digits
     return ''.join(random.choices(alphanumeric, k=id_length))
+
+def split_data(data, train_percent=0.8, test_percent=0.2):
+    train, test = train_test_split(data, train_size=train_percent, shuffle=True)
+    return train, test
